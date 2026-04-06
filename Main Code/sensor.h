2a3919bfd3be5131a7a2330ee4b06945370e619a
EastonK-04 <eastonkennedy@icloud.com>
2026-04-06 14:16:04 -0500

    Update sensor.h

--- Main Code/sensor.h
+++ Main Code/sensor.h
@@ -12,18 +12,26 @@
 #include <stdint.h>
 
 int barom_init(void);
-int barom_read(float *p_pres, float *p_temp);
+int barom_read(float *p_hpa);
 
 int imu_init(void);
 int imu_read(int16_t lowG[3], int16_t highG[3], int16_t gyro[3]);
 
-void flash_page_program(uint32_t addr, uint8_t *data, uint16_t len);
-void flash_read(uint32_t addr, uint8_t *data, uint16_t len);
+int flash_page_program(uint32_t addr, const uint8_t *data, uint16_t len);
+int flash_read(uint32_t addr, uint8_t *data, uint16_t len);
 void flash_init(void);
 void flash_sector_erase(uint32_t addr);
 uint8_t flash_read_status(void);
-void flash_write_txbuf(uint32_t *flash	_write_addr, uint8_t *tx_buf, uint32_t len);
+void flash_erase_entire_chip(void);
+void flash_wait_busy(void);
+uint8_t flash_append_packet(const uint8_t *packet, uint16_t len);
+uint8_t flash_is_busy(void);
+int flash_write_start(uint32_t addr, const uint8_t *data, uint16_t len);
 
+void service_ring(void);
+void ring_buf_init(void);
+uint8_t write_ring_buf(const uint8_t *packet, uint16_t len);
+uint8_t flash_ring_overflowed(void);
 
 void gps_init(void);
 int gps_gga(const char *gga,
